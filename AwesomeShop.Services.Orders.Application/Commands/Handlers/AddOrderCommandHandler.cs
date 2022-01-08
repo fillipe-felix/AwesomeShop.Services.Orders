4047c2b9b66967017cc602a7b2bd2f4821db8ec7
fillipe.felix <felipesoares_1993@hotmail.com>
2022-01-08 15:27:13 -0300

    Criando e implementando o command de AddOrder e seu handler

---
+++ AwesomeShop.Services.Orders.Application/Commands/Handlers/AddOrderCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AwesomeShop.Services.Orders.Core.Repositories;
+
+using MediatR;
+
+namespace AwesomeShop.Services.Orders.Application.Commands.Handlers
+{
+    public class AddOrderCommandHandler : IRequestHandler<AddOrderCommand, Guid>
+
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public AddOrderCommandHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Guid> Handle(AddOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = request.ToEntity();
+
+            await _orderRepository.AddAsync(order);
+
+            return order.Id;
+        }
+    }
+}
