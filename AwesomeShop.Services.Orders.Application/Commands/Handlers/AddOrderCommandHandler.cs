c81a32bc5730985ab89812b289303f1d9d74e87b
fillipe.felix <felipesoares_1993@hotmail.com>
2022-01-12 21:38:26 -0300

    adicionando a publicação da mensagem de um novo pedido na fila do rabbitmq pelo exchange "order-service"

--- AwesomeShop.Services.Orders.Application/Commands/Handlers/AddOrderCommandHandler.cs
+++ AwesomeShop.Services.Orders.Application/Commands/Handlers/AddOrderCommandHandler.cs
@@ -3,6 +3,8 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using AwesomeShop.Services.Orders.Core.Repositories;
+using AwesomeShop.Services.Orders.Infrastructure;
+using AwesomeShop.Services.Orders.Infrastructure.MessageBus;
 
 using MediatR;
 
@@ -12,10 +14,12 @@ namespace AwesomeShop.Services.Orders.Application.Commands.Handlers
 
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly IMessageBusClient _messageBus;
 
-        public AddOrderCommandHandler(IOrderRepository orderRepository)
+        public AddOrderCommandHandler(IOrderRepository orderRepository, IMessageBusClient messageBus)
         {
             _orderRepository = orderRepository;
+            _messageBus = messageBus;
         }
 
         public async Task<Guid> Handle(AddOrderCommand request, CancellationToken cancellationToken)
@@ -23,6 +27,13 @@ namespace AwesomeShop.Services.Orders.Application.Commands.Handlers
             var order = request.ToEntity();
 
             await _orderRepository.AddAsync(order);
+
+            foreach (var @event in order.Events)
+            {
+                //OrderCreated = order-created
+                var rountingKey = @event.GetType().Name.ToDashCase();
+                _messageBus.Publish(@event, rountingKey, "order-service");
+            }
 
             return order.Id;
         }
